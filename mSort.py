edbd222579962d638afc4819690060070023ac2c
Harley Dutton <harleydutton@gmail.com>
2018-11-24 13:05:25 -0700

    finished with setup phase 2

--- mSort.py
+++ mSort.py
@@ -28,15 +28,19 @@ if os.path.isdir(datafolder):
             settings = json.loads(f.read())
             musicfolder = settings['musicfolder']
             musicfoldername = settings['musicfoldername']
+            f.close()
     else:
         initSettings()
 else:
     os.makedirs(datafolder)
     initSettings()
+if not os.path.isdir(musicfolder):
+    print('That folder does not exits.')
+    os.remove(settingsfile)
+    exit()
 repairfolder = rootpath+sep+musicfoldername+'-repair'
 upvotefolder = rootpath+sep+musicfoldername+'-upvote'
 downvotefolder = rootpath+sep+musicfoldername+'-downvote'
-
 print('root path is {}'.format(rootpath))
 print('data folder is {}'.format(datafolder))
 print('settings file is {}'.format(settingsfile))
@@ -45,7 +49,14 @@ print('music folder name is {}'.format(musicfoldername))
 print('upvote folder is {}'.format(upvotefolder))
 print('downvote folder is {}'.format(downvotefolder))
 print('repair folder is {}'.format(repairfolder))
-
+if not os.path.isdir(repairfolder):
+    os.makedirs(repairfolder)
+if not os.path.isdir(upvotefolder):
+    os.makedirs(upvotefolder)
+if not os.path.isdir(downvotefolder):
+    os.makedirs(downvotefolder)
+#write a separate program to rename all the files such that they are printable
+#then make a list, shuffle it, and begin writing the actual program.
 
 
 
