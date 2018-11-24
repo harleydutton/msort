cb9e9b20902f5dfc6da796ece422f97e48b4f1f3
Harley Dutton <harleydutton@gmail.com>
2018-11-24 11:34:11 -0700

    first commit of the day

--- mSort.py
+++ mSort.py
@@ -8,24 +8,30 @@ settingsfile = datafolder+sep+'settings.json'
 print('root path is {}'.format(rootpath))
 print('data folder is {}'.format(datafolder))
 print('settings file is {}'.format(settingsfile))
+musicfolder = ''
+settings = {}
 def initSettings():
+    global musicfolder
     musicfolder = rootpath+sep+input('What is the name of the folder with your music?\n')
-    settings = {}
+    global settings
     settings['musicfolder'] = musicfolder
     json_data = json.dumps(settings)
-    #save the json data to settingsfile
-    print(json_data)
+    f = open(settingsfile,"w+")
+    f.write(json_data)
+    f.close()
 if os.path.isdir(datafolder):
     if os.path.isfile(settingsfile):
         #load up the file options/settins/temp file and set the music folder.
-        a = 0
+        f = open(settingsfile,'r')
+        if f.mode == 'r':
+            settings = json.loads(f.read())
+            musicfolder = settings['musicfolder']
     else:
         initSettings()
 else:
     os.makedirs(datafolder)
     initSettings()
 print(musicfolder)
-#currently running into a scope issue: musicfolder ceases to be
 
 
 
